70f2f350f8616d44a6e52d599f16087ac8b7f52f
udayshnakar98 <84951394+udayshnakar98@users.noreply.github.com>
2021-06-09 11:41:45 +0530

    Add files via upload

--- movezeroes.py
+++ movezeroes.py
@@ -2,7 +2,7 @@ def movezeros(n,data):
     for i in data:
         if i==0:
             data.remove(i)
-            data.append(0)
+            data.append(i)
 
 n=int(input())
 data=list(map(int,input().split()))
